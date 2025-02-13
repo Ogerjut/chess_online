7d219cf295de8f294e29baee6b82e5a1cba0f718
Ogerjut <vallereau.robin@gmail.com>
2025-02-13 19:39:22 +0100

    first commit

---
+++ src/lib/components/Pieces.svelte
@@ -0,0 +1,55 @@
+<script>
+import { draggable } from "$lib/dnd";
+
+    export let type;
+    export let color;
+    export let x;
+    export let y;
+
+    // Dictionnaire des positions dans la sprite sheet
+    const pieceMap = {
+        pawn: 5,
+        rook: 4,
+        knight: 2,
+        bishop: 3,
+        queen: 1,
+        king: 0
+    };
+
+    // Coordonnées dans la sprite
+    const spriteX = pieceMap[type] * 64;
+    const spriteY = color === "black" ? 64 : 0;
+
+    // function handleDragStart(e) {
+    //     console.log("début drag")
+    //     console.log(e)
+    //     e.dataTransfer.setData("text/plain",`${x}, ${y}`)
+    // }
+
+
+
+    // function handleDragEnd(e) {
+    //     console.log('end drag : mouse release or esc')
+    // }
+</script>
+
+<div class="piece"
+    use:draggable={{x , y}}
+    style="
+    left: {x * 64}px;
+    top: {y * 64}px;
+    background-position: {-spriteX}px {-spriteY}px;
+"
+>
+
+</div>
+
+<style>
+    .piece {
+        width: 64px;
+        height: 64px;
+        position: absolute;
+        background-image: url('pieces_sprite.png');
+        background-size: 384px 128px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+
+<script>
+    import Board from "$lib/components/Board.svelte";
+    import Pieces from "$lib/components/Pieces.svelte";
+</script>
+
+<h1>Online chess game</h1>
+
+<Board/>
+<!-- <Pieces/> -->
+
